a6dbd496f5c39568b4894c0eaf92848ce96b3cab
s-yamauchi07 <hand1nhand306@gmail.com>
2023-05-01 15:56:40 +0900

    ビューの作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,86 @@
+import { useState, userEffect, createContext} from 'react'
+import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
+
+import Home from './components/Home';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+import { getCurrentUser} from './api/auth';
+
+export const AuthContext = createContext();
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [isSignedIn, setIsSignedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState();
+
+  // 認証済み(登録)のユーザーがどうかをチェック。確認できた場合は、そのユーザー情報を取得。
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser()
+
+      if (res?.data.isLogin === true) {
+        setIsSignedIn(true);
+        setCurrentUser(res?.data.data)
+
+        console.log(res?.data.data)
+      } else {
+        console.log("No current user")
+      }
+    } catch (err) {
+      console.log(err)
+    }
+
+    setLoading(false)
+  }
+
+  //初回とsetCurrentUserの値に変化があったときにhandleGetCurrentUserを実行。
+  userEffect(() => {
+    handleGetCurrentUser()
+  }, [setCurrentUser])
+
+  //ユーザーが認証済みかどうかでルーティングを決定する。
+  //未承認だった場合は、signinページへ遷移させる。
+  const Private = ({children}) => {
+    if(!loading) {
+      if(isSignedIn) {
+        return children;
+      } else {
+      return <Redirect to="signin" />;
+      }
+    } else {
+      return <></>;
+    }
+  };
+
   return (
-    <h1>Hello</h1>
+      <AuthContext.Provider
+      value={{
+        loading,
+        setLoading,
+        isSignedIn,
+        setIsSignedIn,
+        currentUser,
+        setCurrentUser,
+      }}
+      >
+        {/* ログイン時にはHomeコンポーネント、非ログイン時にはsignup,signinのページを表示させる。 */}
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/signup">
+              <SignUp />
+            </Route>
+            <Route exact path="/signin">
+              <SignUp />
+            </Route>
+            <Private>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Private>
+          </Switch>
+        </BrowserRouter>
+      </AuthContext.Provider>
   );
 }
 
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,3 @@
+export const SignIn = () => {
+  return <p>SignIn</p>
+} 
