cd33467ac8bd91a7bca58aa8f952e4840347fddb
s-yamauchi07 <hand1nhand306@gmail.com>
2023-05-08 16:10:55 +0900

    リンクの設定

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,43 @@
-import { NavLink} from "react-router-dom";
-
 import * as React from 'react';
+
+import { useHistory, Link } from "react-router-dom";
+import { useContext} from 'react'
+import {AuthContext} from '../App'
+import { signOut } from "../api/auth";
+import Cookies from "js-cookie";
+
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export const Header = () => {
+  const { isSignedIn, setIsSignedIn, currentUser } = useContext(AuthContext)
+  const history = useHistory()
+
+  const handleSignOut = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await signOut();
+      if (res.data.success === true) {
+        Cookies.remove("_access_token")
+        Cookies.remove("_client")
+        Cookies.remove("_uid")
+
+        setIsSignedIn(false)
+        history.push("/")
+        console.log("succeeded in signed out")
+      } else {
+        console.log("Failed tin sign out")
+      }
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   return (
     <>
     <Box sx={{ flexGrow: 1 }}>
@@ -23,10 +52,18 @@ export const Header = () => {
           >
           <MenuIcon />
           </IconButton>
-            <NavLink to="/signin">Login</NavLink>
-            <NavLink to="/signup">Signup</NavLink>
-            <NavLink to="/">Name</NavLink>
-            <NavLink to="/">LogOut</NavLink>
+
+          {isSignedIn ? (
+            <>
+             <Button sx={{color: '#fff'}}>{currentUser.nickname}</Button>
+             <Button sx={{color: '#fff'}} onClick={handleSignOut}>LogOut</Button>
+            </>
+          ) : (
+            <>
+            <Button component={Link} to="/signin">Login</Button>
+            <Button component={Link} to="/signup">Signup</Button>
+            </>
+            )}
         </Toolbar>
       </AppBar>
     </Box>
