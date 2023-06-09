8bf4b1c810cdd5f5dd21261c02b681e819ef7a10
s-yamauchi07 <hand1nhand306@gmail.com>
2023-06-09 12:31:22 +0900

    フッターの作成

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+export const Footer = () => {
+  return(
+    <>
+    <div style={
+      { height: '64px',
+        display: 'flex',
+        justifyContent:'center',
+        alignItems: 'center',
+        backgroundColor: '#1976d2',
+        color: '#fff'
+      }
+    }>
+      <p>©️2023 piggy bank 365</p>
+    </div>
+    </>
+  )
+}
