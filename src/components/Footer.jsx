a18dcf73e5dbc063a2281dce67127387f53c4267
s-yamauchi07 <hand1nhand306@gmail.com>
2023-06-22 13:02:27 +0900

    レイアウトの修正

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,15 @@ export const Footer = () => {
     <>
     <div style={
       { height: '64px',
+        width: '100vw',
         display: 'flex',
         justifyContent:'center',
         alignItems: 'center',
         backgroundColor: '#1976d2',
-        color: '#fff'
+        color: '#fff',
+        position: 'absolute',
+        bottom: '0',
+        left: '0',
       }
     }>
       <p>©️2023 piggy bank 365</p>
